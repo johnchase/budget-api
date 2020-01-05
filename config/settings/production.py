df3d1bfe51430f0638259de5c6aded964e141671
John Chase <chasejohnh@gmail.com>
2020-01-05 09:06:43 -0800

    added ip to settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,5 +1,5 @@
 from .base import *  # noqa: F403, F401
 
-ALLOWED_HOSTS = ['budget-production.us-west-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['172.31.16.41', 'budget-production.us-west-1.elasticbeanstalk.com']
 
 DEBUG = True
