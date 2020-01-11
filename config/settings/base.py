823b8fa045550687a59d95dec3257be37a2372ca
John Chase <chasejohnh@gmail.com>
2020-01-10 17:16:59 -0800

    removed cors from base

--- config/settings/base.py
+++ config/settings/base.py
@@ -20,12 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "NotTheKey")
-# SECURITY WARNING: don't run with debug turned on in production!
-
-CORS_ORIGIN_ALLOW_ALL = True
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # Application definition
 
