64901185a1a78a03cd9e9aea960258df7517cc1a
John Chase <chasejohnh@gmail.com>
2020-01-12 16:13:49 -0800

    trying invalid url

--- config/settings/production.py
+++ config/settings/production.py
@@ -6,7 +6,7 @@ DEBUG = False
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ORIGIN_WHITELIST = (
-        "https://budget.johnhchase.com"
+        "https://budgetx.johnhchase.com"
 )
 
 CORS_ALLOW_CREDENTIALS = True
