2529633c0291eca935114065e0edfb2178c7a9ee
John Chase <chasejohnh@gmail.com>
2020-01-21 07:48:59 -0800

    updated settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,8 +22,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 
 # Application definition
 
