0eaf112f8c716cfbae0030c473837a3c2db1e3b6
John Chase <chasejohnh@gmail.com>
2020-01-22 21:10:10 -0800

    middleware

--- budget/middleware.py
+++ budget/middleware.py
@@ -3,6 +3,6 @@ from django.utils.deprecation import MiddlewareMixin
 
 class HealthCheckMiddleware(MiddlewareMixin):
     def process_request(self, request):
-        print('*****************************', request.META)
+        print('*****************************', request.META["PATH_INFO"])
         if request.META["PATH_INFO"] == "'/status/'":
             return HttpResponse(status=200)
