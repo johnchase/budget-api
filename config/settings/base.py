3cc3ab3905fb0701ff680acd969e7af1e9490c65
John Chase <chasejohnh@gmail.com>
2020-01-22 21:49:17 -0800

    updated middleware

--- config/settings/base.py
+++ config/settings/base.py
@@ -41,15 +41,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
-    "budget.middleware.HealthCheckMiddleware",
-    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",]
 
 ROOT_URLCONF = "config.urls"
 
