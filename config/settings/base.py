dc14a1c2eed6f5722abb372fb6a859aa16c08820
John Chase <chasejohnh@gmail.com>
2020-04-19 22:10:19 -0700

    updates

--- config/settings/base.py
+++ config/settings/base.py
@@ -89,14 +89,14 @@ DATABASES = {
 }
 
 REST_FRAMEWORK = {
-    #    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_AUTHENTICATION_CLASSES": (
-        #        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
-        #        "rest_framework.authentication.SessionAuthentication",
-        #        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework_jwt.authentication.JSONWebTokenAuthentication",
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
     ),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 9,
+    "PAGE_SIZE": 7,
 }
 
 # Password validation
